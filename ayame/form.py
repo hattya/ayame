bf46e0c778b7fc83161418b0e085c9f86576535f
Akinori Hattori <hattya@gmail.com>
2011-12-11 18:22:43 +0900

    Add new module ayame.form

---
+++ ayame/form.py
@@ -0,0 +1,218 @@
+#
+# ayame.form
+#
+#   Copyright (c) 2011 Akinori Hattori <hattya@gmail.com>
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation files
+#   (the "Software"), to deal in the Software without restriction,
+#   including without limitation the rights to use, copy, modify, merge,
+#   publish, distribute, sublicense, and/or sell copies of the Software,
+#   and to permit persons to whom the Software is furnished to do so,
+#   subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+#   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+#   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+#   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+#
+
+from __future__ import unicode_literals
+import collections
+
+from ayame import core, markup, uri, validator
+from ayame.exception import ComponentError, ConversionError, RenderingError
+from ayame.exception import ValidationError
+
+
+__all__ = ['Form', 'FormComponent', 'Button', 'TextField']
+
+# HTML elements
+_FORM = markup.QName(markup.XHTML_NS, 'form')
+_INPUT = markup.QName(markup.XHTML_NS, 'input')
+_BUTTON = markup.QName(markup.XHTML_NS, 'button')
+
+# HTML attributes
+_CLASS = markup.QName(markup.XHTML_NS, 'class')
+
+_ACTION = markup.QName(markup.XHTML_NS, 'action')
+_METHOD = markup.QName(markup.XHTML_NS, 'method')
+_TYPE = markup.QName(markup.XHTML_NS, 'type')
+_NAME = markup.QName(markup.XHTML_NS, 'name')
+_VALUE = markup.QName(markup.XHTML_NS, 'value')
+
+class Form(core.MarkupContainer):
+
+    def __init__(self, id, model=None):
+        super(Form, self).__init__(id, model)
+        self._method = None
+
+        self.add(_FormActionBehavior())
+
+    def on_render(self, element):
+        if element.qname != _FORM:
+            raise RenderingError(self, "'form' element is expected")
+        elif _METHOD not in element.attrib:
+            raise RenderingError(
+                    self, "'method' attribute is required for 'form' element")
+
+        # modify attributes
+        self._method = element.attrib[_METHOD].upper()
+        element.attrib[_ACTION] = unicode(uri.request_path(self.environ))
+        element.attrib[_METHOD] = self._method.lower()
+        # insert hidden field for marking
+        div = markup.Element(markup.DIV)
+        div.attrib[_CLASS] = 'ayame-hidden'
+        input = markup.Element(_INPUT, type=markup.Element.EMPTY)
+        input.attrib[_TYPE] = 'hidden'
+        input.attrib[_NAME] = unicode(core.AYAME_PATH)
+        input.attrib[_VALUE] = unicode(self.path())
+        div.children.append(input)
+        element.children.insert(0, div)
+        # render form
+        return super(Form, self).on_render(element)
+
+    def submit(self, request):
+        if (request.method != self._method and
+            not self.on_method_mismatch()):
+            return  # abort
+        elif request.method == 'GET':
+            values = request.query
+        elif request.method == 'POST':
+            values = request.body
+        else:
+            return  # unknown method
+
+        form = button = None
+        queue = collections.deque()
+        queue.append(self)
+        while queue:
+            component = queue.pop()
+            # check nested form
+            if isinstance(component, Form):
+                if form is not None:
+                    raise ComponentError(self, "'form' element is nested")
+                form = component
+            # validate
+            if isinstance(component, FormComponent):
+                name = component.relative_path()
+                if isinstance(component, Button):
+                    if (name in values and
+                        button is None):
+                        button = component
+                else:
+                    value = values.get(name)
+                    component.validate(None if value is None else value[0])
+            # push children
+            if isinstance(component, core.MarkupContainer):
+                queue.extend(reversed(component.children))
+        if button is not None:
+            button.on_submit()
+        self.on_submit()
+
+    def on_method_mismatch(self):
+        return True  # continue
+
+    def on_submit(self):
+        pass
+
+class _FormActionBehavior(core.IgnitionBehavior):
+
+    def on_after_render(self, component):
+        self.fire()
+
+    def on_fire(self, component, request):
+        component.submit(request)
+
+class FormComponent(core.MarkupContainer):
+
+    def __init__(self, id, model=None):
+        super(FormComponent, self).__init__(id, model)
+        self.required = False
+        self.type = None
+
+    def relative_path(self):
+        current = self
+        buf = []
+        while not (isinstance(current, Form) or
+                   current.parent is None):
+            buf.append(current.id)
+            current = current.parent
+        if not isinstance(current, Form):
+            raise ComponentError(self, 'component is not attached to Form')
+        return ':'.join(reversed(buf))
+
+    def validate(self, value):
+        try:
+            # check required
+            if (self.required and
+                not value):
+                raise ValidationError()
+            # convert to object
+            if self.type is not None:
+                try:
+                    converter = self.converter_for(self.type)
+                    object = converter.to_python(value)
+                except ConversionError as e:
+                    raise ValidationError(unicode(e))
+            else:
+                object = value
+            # validate
+            for behavior in self.behaviors:
+                if isinstance(behavior, validator.Validator):
+                    behavior.validate(object)
+        except ValidationError:
+            self.on_invalid()
+            raise
+
+        if self.model is not None:
+            self.model.object = object
+        self.on_valid()
+
+    def on_valid(self):
+        pass
+
+    def on_invalid(self):
+        pass
+
+class Button(FormComponent):
+
+    def on_render(self, element):
+        if element.qname == _INPUT:
+            if element.attrib[_TYPE] not in ('submit', 'button', 'image'):
+                raise RenderingError(
+                        self,
+                        "'input' element with 'type' attribute of 'submit', "
+                        "'button' or 'image' is expected")
+        elif element.qname != _BUTTON:
+            raise RenderingError(self,
+                                 "'input' or 'button' element is expected")
+
+        # modify attributes
+        element.attrib[_NAME] = self.relative_path()
+        # render button
+        return super(Button, self).on_render(element)
+
+    def on_submit(self):
+        pass
+
+class TextField(FormComponent):
+
+    input_type = 'text'
+
+    def on_render(self, element):
+        if element.qname != _INPUT:
+            raise RenderingError(self, "'input' element is expected")
+
+        # modify attributes
+        element.attrib[_TYPE] = self.input_type
+        element.attrib[_NAME] = self.relative_path()
+        # render text field
+        return super(TextField, self).on_render(element)
---
+++ tests/test_form.py
@@ -0,0 +1,227 @@
+#
+# test_form
+#
+#   Copyright (c) 2011 Akinori Hattori <hattya@gmail.com>
+#
+#   Permission is hereby granted, free of charge, to any person
+#   obtaining a copy of this software and associated documentation files
+#   (the "Software"), to deal in the Software without restriction,
+#   including without limitation the rights to use, copy, modify, merge,
+#   publish, distribute, sublicense, and/or sell copies of the Software,
+#   and to permit persons to whom the Software is furnished to do so,
+#   subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#i   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+#   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+#   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+#   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+#
+
+from __future__ import unicode_literals
+from contextlib import contextmanager
+import io
+
+from nose.tools import assert_raises, eq_, ok_
+
+from ayame import core, form, http, markup, model, validator
+from ayame.exception import AyameError, ComponentError, RenderingError
+from ayame.exception import ValidationError
+
+
+@contextmanager
+def application(environ=None):
+    local = core._local
+    app = core.Ayame(__name__)
+    app.config['ayame.markup.pretty'] = True
+    try:
+        local.app = app
+        local.environ = environ
+        yield
+    finally:
+        local.environ = None
+        local.app = None
+
+def test_form_error():
+    # not form element
+    f = form.Form('a')
+    assert_raises(RenderingError, f.render, markup.Element(markup.DIV))
+
+    # method is not found
+    root = markup.Element(form._FORM)
+    root.attrib[form._ACTION] = '/'
+    f = form.Form('a')
+    assert_raises(RenderingError, f.render, root)
+
+    # method mismatch
+    class Form(form.Form):
+        def on_method_mismatch(self):
+            return False
+    f = Form('a')
+    f.add(form.FormComponent('b'))
+    query = 'b=1'
+    environ = {'REQUEST_METHOD': 'GET',
+               'SCRIPT_NAME': '',
+               'PATH_INFO': '/form',
+               'QUERY_STRING': query.encode('utf-8')}
+    with application(environ):
+        request = core.Request(environ, {})
+        f._method = 'POST'
+        f.submit(request)
+
+    # unknown method
+    f = form.Form('a')
+    query = 'b=1'
+    environ = {'REQUEST_METHOD': 'PUT',
+               'SCRIPT_NAME': '',
+               'PATH_INFO': '/form',
+               'QUERY_STRING': query.encode('utf-8')}
+    with application(environ):
+        request = core.Request(environ, {})
+        f._method = 'POST'
+        f.submit(request)
+
+    # form is nested
+    f = form.Form('a')
+    f.add(form.Form('b'))
+    environ = {'wsgi.input': io.BytesIO(),
+               'REQUEST_METHOD': 'POST',
+               'SCRIPT_NAME': '',
+               'PATH_INFO': '/form',
+               'CONTENT_TYPE': 'multipart/form-data; boundary=ayame.form'}
+    with application(environ):
+        request = core.Request(environ, {})
+        f._method = 'POST'
+        assert_raises(ComponentError, f.submit, request)
+
+def test_form():
+    content_length = '476'
+    xhtml = ('<?xml version="1.0"?>\n'
+             '{doctype}\n'
+             '<html xmlns="{xhtml}">\n'
+             '  <head>\n'
+             '    <title>SpamPage</title>\n'
+             '  </head>\n'
+             '  <body>\n'
+             '    <form action="/form" method="post">\n'
+             '      <div class="ayame-hidden">'
+             '<input name="{path}" type="hidden" value="form"/></div>\n'
+             '      <input name="text" type="text"/>\n'
+             '      <input name="button" type="submit"/>\n'
+             '    </form>\n'
+             '  </body>\n'
+             '</html>\n').format(doctype=markup.XHTML1_STRICT,
+                                 xhtml=markup.XHTML_NS,
+                                 path=core.AYAME_PATH)
+    model_object = {'text': 'text',
+                    'button': 'submitted'}
+
+    class Button(form.Button):
+        def on_submit(self):
+            super(Button, self).on_submit()
+            self.model_object = 'submitted'
+
+    class SpamPage(core.Page):
+        def __init__(self, request):
+            super(SpamPage, self).__init__(request)
+            self.add(form.Form('form', model.CompoundModel({})))
+            self.find('form').add(form.TextField('text'))
+            self.find('form').add(Button('button'))
+
+    # GET
+    query = ('{}=form&'
+             'text=text&'
+             'button').format(core.AYAME_PATH)
+    environ = {'REQUEST_METHOD': 'GET',
+               'SCRIPT_NAME': '',
+               'PATH_INFO': '/form',
+               'QUERY_STRING': query.encode('utf-8')}
+    with application(environ):
+        request = core.Request(environ, {})
+        page = SpamPage(request)
+        status, headers, body = page.render()
+    eq_(status, http.OK.status)
+    eq_(headers, [('Content-Type', 'text/html; charset=UTF-8'),
+                  ('Content-Length', content_length)])
+    eq_(body, xhtml)
+    eq_(page.find('form').model_object, model_object)
+
+    # POST
+    body = ('--ayame.form\r\n'
+            'Content-Disposition: form-data; name="{}"\r\n'
+            '\r\n'
+            'form\r\n'
+            '--ayame.form\r\n'
+            'Content-Disposition: form-data; name="text"\r\n'
+            '\r\n'
+            'text\r\n'
+            '--ayame.form\r\n'
+            'Content-Disposition: form-data; name="button"\r\n'
+            '\r\n'
+            '--ayame.form--\r\n'
+            '\r\n').format(core.AYAME_PATH)
+    environ = {'wsgi.input': io.BytesIO(body.encode('utf-8')),
+               'REQUEST_METHOD': 'POST',
+               'SCRIPT_NAME': '',
+               'PATH_INFO': '/form',
+               'CONTENT_TYPE': 'multipart/form-data; boundary=ayame.form'}
+    with application(environ):
+        request = core.Request(environ, {})
+        page = SpamPage(request)
+        status, headers, body = page.render()
+    eq_(status, http.OK.status)
+    eq_(headers, [('Content-Type', 'text/html; charset=UTF-8'),
+                  ('Content-Length', content_length)])
+    eq_(body, xhtml)
+    eq_(page.find('form').model_object, model_object)
+
+def test_form_component():
+    # relative path
+    f = form.Form('a')
+    f.add(form.FormComponent('b1'))
+    f.add(core.MarkupContainer('b2'))
+    f.find('b2').add(form.FormComponent('c'))
+    eq_(f.find('b1').relative_path(), 'b1')
+    eq_(f.find('b2').find('c').relative_path(), 'b2:c')
+    assert_raises(ComponentError, form.FormComponent('a').relative_path)
+
+    # required error
+    fc = form.FormComponent('a')
+    fc.required = True
+    assert_raises(ValidationError, fc.validate, None)
+    assert_raises(ValidationError, fc.validate, '')
+
+    # conversion error
+    with application():
+        fc = form.FormComponent('a')
+        fc.type = int
+        assert_raises(ValidationError, fc.validate, 'a')
+
+    # validation error
+    with application():
+        fc = form.FormComponent('a')
+        fc.add(validator.StringValidator(max=4))
+        assert_raises(ValidationError, fc.validate, '.info')
+
+    # no model
+    with application():
+        fc = form.FormComponent('a')
+        fc.validate('a')
+        eq_(fc.model, None)
+        eq_(fc.model_object, None)
+
+def test_invalid_markup():
+    input = markup.Element(form._INPUT)
+    input.attrib[form._TYPE] = 'text'
+    button = form.Button('a')
+    assert_raises(RenderingError, button.render, input)
+    assert_raises(RenderingError, button.render, markup.Element(markup.DIV))
+
+    tf = form.TextField('a')
+    assert_raises(RenderingError, tf.render, markup.Element(markup.DIV))
