bde75670b46ac8dfc93596dc57de804748a9fd0f
Akinori Hattori <hattya@gmail.com>
2022-10-02 11:05:53 +0900

    Migrate to PEP 517

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ class test(Command):
     def run(self):
         import unittest
 
-        self.distribution.fetch_build_eggs(self.distribution.install_requires)
         self.run_command('egg_info')
         # run unittest discover
         argv = [sys.argv[0], 'discover', '--start-directory', 'tests']
@@ -36,53 +35,14 @@ class test(Command):
         unittest.main(None, argv=argv)
 
 
-try:
-    with open('README.rst') as fp:
-        long_description = fp.read()
-except OSError:
-    long_description = ''
-
-packages = ['ayame']
-package_data = {
-    'ayame': ['*/*.html'],
-}
-
 cmdclass = {
     'test': test,
 }
 
-setup(name='ayame',
-      description='An Apache Wicket-like component based WSGI framework',
-      long_description=long_description,
-      author='Akinori Hattori',
-      author_email='hattya@gmail.com',
-      url='https://github.com/hattya/ayame',
-      license='MIT',
-      packages=packages,
-      package_data=package_data,
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-          'Topic :: Internet :: WWW/HTTP :: WSGI',
-          'Topic :: Software Development :: Libraries :: Application Frameworks',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
-      python_requires='>= 3.7',
-      cmdclass=cmdclass,
-      setup_requires=['scmver'],
+setup(cmdclass=cmdclass,
       scmver={
           'root': os.path.dirname(os.path.abspath(__file__)),
           'spec': 'micro',
           'write_to': os.path.join('ayame', '__version__.py'),
           'fallback': ['__version__:version', 'ayame'],
-      },
-      install_requires=['Werkzeug', 'secure-cookie'])
+      })
